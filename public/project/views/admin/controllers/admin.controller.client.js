678cdbc0ac3d6152ba1c72f1fc8f79ea5a92b913
apreethi <preeth.ceg@gmail.com>
2017-08-18 05:54:25 -0400

    Pagination and category fix

--- public/project/views/admin/controllers/admin.controller.client.js
+++ public/project/views/admin/controllers/admin.controller.client.js
@@ -3,11 +3,19 @@
         .module("GroceryApp")
         .controller("AdminController", AdminController);
     
-    function AdminController(categoryService,$location,paginatedService,CurrentUser) {
+    function AdminController(categoryService,CurrentUser) {
         var model = this;
+        model.updateCategories=updateCategories;
         function init() {
             model.currentUser=CurrentUser;
+            model.isUpdated=false;
         }
 
         init();
+        function updateCategories(){
+            categoryService.updateCategories()
+                .then (function (response) {
+                   model.isUpdated=true;
+                });
+        }
     }})();
--- public/project/views/home/home.controller.client.js
+++ public/project/views/home/home.controller.client.js
@@ -2,22 +2,22 @@
     angular
         .module("GroceryApp")
         .controller("homeController", homeController);
-    
-    function homeController(categoryService,$location,paginatedService,CurrentUser,UserService,$route) {
+
+    function homeController(categoryService, $location, paginatedService, CurrentUser, UserService, $route) {
         var model = this;
-        model.updateCategories=updateCategories;
-        model.setSelected=setSelected;
-        model.setSubCategory=setSubCategory;
-        model.loadProduct=loadProduct;
-        model.logout=logout;
-        model.setSubset=setSubset;
-        model.currentState=currentState;
+        model.updateCategories = updateCategories;
+        model.setSelected = setSelected;
+        model.setSubCategory = setSubCategory;
+        model.loadProduct = loadProduct;
+        model.logout = logout;
+        model.setSubset = setSubset;
+        model.currentState = currentState;
         function init() {
             model.currentUser = CurrentUser;
             model.itemsPerPage = 20;
             model.categoryName;
-            model.initial_load=true;
-            model.currentSubcategory="Choose/Change Categories";
+            model.initial_load = true;
+            model.currentSubcategory = "Choose/Change Categories";
             categoryService.getCategories()
                 .then(function (response) {
                     model.categories = response;
@@ -26,73 +26,78 @@
                     setSelected(model.categories[0].children[0]);
                 });
         }
+
         init();
-      function updateCategories(){
+        function updateCategories() {
             categoryService.updateCategories()
-                .then (function (response) {
+                .then(function (response) {
                     location.reload()
                 });
         }
-            function setSubset(index){
-               var current_start=parseInt(index*model.itemsPerPage);
-               var current_end=current_start+model.itemsPerPage;
-               if(current_end > model.totalproducts )
-                   current_end=model.totalproducts;
-                model.productSubset = model.products.slice(current_start, current_end);
-                document.body.scrollTop = document.documentElement.scrollTop = 0;
 
-            }
+        function setSubset(index) {
+            var current_start = parseInt(index * model.itemsPerPage);
+            var current_end = current_start + model.itemsPerPage;
+            if (current_end > model.totalproducts)
+                current_end = model.totalproducts;
+            model.productSubset = model.products.slice(current_start, current_end);
+            document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+        }
+
         function logout() {
             UserService
                 .logout()
                 .then(
-                    function(response) {
+                    function (response) {
                         $route.reload();
                     });
         }
 
-        function setSubCategory(category){
-            model.categoryName=category.name;
-            model.currentSubcategory="Choose/Change Categories";
-            if(category.children.length >0 )
-            {
-                model.subCategory=category.children;
+        function setSubCategory(category) {
+            model.categoryName = category.name;
+            model.currentSubcategory = "Choose/Change Categories";
+            if (category.children.length > 0) {
+                model.subCategory = category.children;
                 setSelected(category.children[0]);
             }
-            else
-            {
-                model.subCategory=[];
+            else {
+                model.subCategory = [];
                 paginatedService.getProducts(category.id)
                     .then(function (response) {
-                        model.products=response;
-                        model.totalproducts=model.products.length;
-                        model.pagination= new Array(model.totalproducts/model.itemsPerPage);
-                        setSubset(0);
+                        model.products = response;
+                        model.totalproducts = model.products.length;
+                        if(model.totalproducts)
+                        {model.pagination = new Array(Math.ceil(model.totalproducts / model.itemsPerPage));
+                        setSubset(0);}
                     });
             }
         }
 
 
         function setSelected(category) {
-            model.initial_load=false;
-            model.currentSubcategory=category.name;
+            model.initial_load = false;
+            model.currentSubcategory = category.name;
             paginatedService.getProducts(category.id)
                 .then(function (response) {
-                    model.products=response;
-                    model.totalproducts=model.products.length;
-                    model.pagination= new Array(model.totalproducts/model.itemsPerPage);
-                    setSubset(0);
+                    model.products = response;
+                    model.totalproducts = model.products.length;
+                    if(model.totalproducts){
+
+                        model.pagination = new Array(Math.ceil(model.totalproducts / model.itemsPerPage));
+                    setSubset(0);}
                 });
         }
 
-        function currentState(current_index,my_index) {
-            if(current_index===my_index){
+        function currentState(current_index, my_index) {
+            if (current_index === my_index) {
                 return "active";
-            }else
+            } else
                 return "";
         }
-        function loadProduct(productId){
-            $location.url("/product/"+productId);
+
+        function loadProduct(productId) {
+            $location.url("/product/" + productId);
 
         }
 
